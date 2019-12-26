458ff3aeed37a787995eecb3d337e965833bb999
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2019-12-26 16:30:45 -0500

    Top level BUILD now builds everything.

--- build.bat
+++ build.bat
@@ -3,7 +3,13 @@ rem
 rem   Build everything from this source directory.
 rem
 setlocal
-call godir "(cog)source/sst"
+call godir "(cog)source/sst/r_pas"
+call build
+call godir "(cog)source/sst/r_syn"
+call build
+call godir "(cog)source/sst/w_c"
+call build
 
+call godir "(cog)source/sst"
 call build_lib
 call build_progs
